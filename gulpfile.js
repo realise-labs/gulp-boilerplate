c8958ef0ddf3cac3730cfe9c5fbe633ad5f7f49d
jameshaddrill <jameshaddrill@gmail.com>
2017-08-15 14:51:30 +0100

    SASS updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var postCSS = require('gulp-postcss');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	rename = require('gulp-rename'),
+	sourcemaps = require('gulp-sourcemaps'),
+	postcss = require('gulp-postcss'),
+
+	sequence = require('run-sequence'),
+
+	// postcss plugins
+	autoprefixer = require('autoprefixer'),
+	reporter = require('postcss-reporter'),
+	stylelint = require('stylelint'),
+	cssnano = require('cssnano'),
+	syntax_scss = require('postcss-scss');
 
 var settings = {
 	paths: {
@@ -18,10 +25,46 @@ var settings = {
 	}
 };
 
+gulp.task('scss-lint', function() {
+	return gulp.src([settings.paths.input.styles, '!src/sass/main.scss', '!src/sass/components/_components.scss', '!src/sass/common/_base.scss', '!src/sass/vendor/**/*.scss'])
+		.pipe(postcss([
+            stylelint(),
+            reporter({ clearMessages: true, throwError: true, noIcon: false })
+        ], { syntax: syntax_scss }))
+
+});
 
 
-gulp.task('develop', function(callback) {});
+gulp.task('sass-develop', function() {
+	return gulp.src(settings.paths.input.styles)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([
+			autoprefixer({browsers: ['> 1%'], cascade: false}),
+			reporter({ clearMessages: true, throwError: true, noIcon: false })
+		]))
+		.pipe(gulp.dest(settings.paths.output.styles));
+});
 
-gulp.task('build', function(callback) {});
+gulp.task('sass-build', function() {
+	return gulp.src(settings.paths.input.styles)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(postcss([
+			cssnano,
+			autoprefixer({browsers: ['> 1%'], cascade: false}),
+			reporter({ clearMessages: true, throwError: true, noIcon: false })
+		]))
+		.pipe(gulp.dest(settings.paths.output.styles))
+});
+
+
+gulp.task('develop', function(callback) {
+	sequence('scss-lint','sass-develop');
+});
+
+gulp.task('build', function(callback) {
+	sequence('scss-lint','sass-develop');
+});
 
 gulp.task('default', ['develop']);
