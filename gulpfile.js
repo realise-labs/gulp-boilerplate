be1d633567da71f495c51bb38059d980fb3b9ce5
jameshaddrill <jameshaddrill@gmail.com>
2017-08-15 09:33:03 +0100

    Added basic sass functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var postCSS = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
+
 var settings = {
+	paths: {
+		input: {
+			styles: 'src/sass/**/*.scss'
+		},
+
+		output: {
+			styles: 'dist/css'
+		}
+	}
 };
 
 
