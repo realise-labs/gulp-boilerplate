494af707f6e40dbbcd242123a93bf495aeeea951
David Storey <david@lostincode.co.uk>
2017-08-23 08:34:48 +0100

    Adding babelify

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+/* This is the starting point of the code, everything gets kicked off here */
+
+'use strict';
+
+import hello from './module';
+
+hello('world');
---
+++ src/scripts/module.js
@@ -0,0 +1,7 @@
+/* Just an example of importing from another module */
+
+'use strict';
+
+export default function hello(name) {
+	alert('Hello ' + name);
+};
