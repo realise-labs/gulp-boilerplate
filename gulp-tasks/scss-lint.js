517442340532e34dd19e5f33d48303054d96e577
David Storey <david@lostincode.co.uk>
2017-09-18 09:09:10 +0100

    Fixed issue with lintedFiles variable

--- gulp-tasks/scss-lint.js
+++ gulp-tasks/scss-lint.js
@@ -1,14 +1,4 @@
 module.exports = function(gulp, plugins, config) {
-	var lintedFiles = [
-		config.paths.input.styles,
-		'!src/sass/main.scss',
-		'!src/sass/common/_spritesheet.scss',
-		'!src/sass/components/_components.scss',
-		'!src/sass/common/_base.scss',
-		'!src/sass/common/_mixins.scss',
-		'!src/sass/vendor/**/*.scss'
-	]
-
 	gulp.task('scss-lint', function() {
 		return gulp.src(config.lintedFiles)
 			.pipe(plugins.postcss([
