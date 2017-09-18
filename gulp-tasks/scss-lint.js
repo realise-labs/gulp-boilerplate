2ad508d86ac01c2b8678068d6c00e17ed2d24c47
jameshaddrill <jameshaddrill@gmail.com>
2017-09-18 14:53:14 +0100

    Update

--- gulp-tasks/scss-lint.js
+++ gulp-tasks/scss-lint.js
@@ -1,16 +1,16 @@
 module.exports = function(gulp, plugins, config) {
 	var lintedFiles = [
 		config.paths.input.styles,
-		'!src/sass/main.scss',
-		'!src/sass/common/_spritesheet.scss',
-		'!src/sass/components/_components.scss',
-		'!src/sass/common/_base.scss',
-		'!src/sass/common/_mixins.scss',
-		'!src/sass/vendor/**/*.scss'
+		'!' + config.paths.input.stylesFolder + '/main.scss',
+		'!' + config.paths.input.stylesFolder + '/common/_spritesheet.scss',
+		'!' + config.paths.input.stylesFolder + '/components/_components.scss',
+		'!' + config.paths.input.stylesFolder + '/common/_base.scss',
+		'!' + config.paths.input.stylesFolder + '/common/_mixins.scss',
+		'!' + config.paths.input.stylesFolder + '/vendor/**/*.scss'
 	]
 
 	gulp.task('scss-lint', function() {
-		return gulp.src(config.lintedFiles)
+		return gulp.src(lintedFiles)
 			.pipe(plugins.postcss([
 				plugins.stylelint(),
 				plugins.postcssReporter({ clearReportedMessages: true, throwError: true, noIcon: false, plugins: ['stylelint'] })
