dfc30d289c41d023b5e6266864f60de8786e0734
jameshaddrill <jameshaddrill@gmail.com>
2017-09-22 09:49:52 +0100

    HTML linting

---
+++ gulp-tasks/html-lint.js
@@ -0,0 +1,27 @@
+module.exports = function(gulp, plugins, config, gutil) {
+	gulp.task('html-lint', function() {
+	    return gulp.src('dist/index.html')
+	        .pipe(plugins.htmllint({
+	        	"rules" : {
+	        		'doctype-first' : true,
+	        		'line-end-style' : false,
+	        		'attr-name-style' : 'dash',
+	        		'doctype-html5' : true,
+	        		'html-req-lang' : true,
+	        		'id-class-style' : 'dash',
+	        		'img-req-alt' : 'allownull',
+	        		'input-req-label' : true
+	        	}
+	        }, htmllintReporter));
+	});
+
+	function htmllintReporter(filepath, issues) {
+	    if (issues.length > 0) {
+	        issues.forEach(function (issue) {
+	            gutil.log(gutil.colors.cyan('[gulp-htmllint] ') + gutil.colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + gutil.colors.red('(' + issue.code + ') ' + issue.msg));
+	        });
+
+	        process.exitCode = 1;
+	    }
+	}
+};
