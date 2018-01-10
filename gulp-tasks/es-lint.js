23ec7b83d60eff86cdf9dacf3a839ecc8bf8df8d
David Storey <david.storey@realise.com>
2018-01-10 10:28:25 +0000

    Changing where eslint rules are set
    Was falling back to user specific version which wasn't always appropriate

--- gulp-tasks/es-lint.js
+++ gulp-tasks/es-lint.js
@@ -1,23 +1,7 @@
 module.exports = function (gulp, plugins, config, errorHandler) {
 	gulp.task('es-lint', function() {
         return gulp.src([config.paths.input.scripts + '**/*.js', '!' + config.paths.input.scripts + 'vendor/**/*.js'])
-            .pipe(plugins.eslint({
-                'extends': 'eslint:recommended',
-                'parserOptions': {
-                    'ecmaVersion': 6,
-                    'sourceType': 'module'
-                },
-                'rules': {
-                    // Full list of rules here: https://eslint.org/docs/rules/
-                    'camelcase': 'warn',
-                    'curly': 'warn',
-                    'eqeqeq': 'warn',
-                    'indent': ['warn', 'tab'],
-                    'no-extra-semi': 'warn',
-                    'quotes': ['warn', 'single'],
-                    'semi': 'warn'
-                }
-            }))    
+            .pipe(plugins.eslint())    
             .pipe(plugins.eslint.format());
 	});
 };
