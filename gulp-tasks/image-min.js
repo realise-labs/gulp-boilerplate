86dc8c408c41cd6bbd49cbe6a488be02d309bb9c
Bee Flaherty <beeflaherty@gmail.com>
2018-01-22 15:35:44 +0000

    Is image minification working correctly? #44
    No.  Added plugins for PNG, SVG and Gif files.

--- gulp-tasks/image-min.js
+++ gulp-tasks/image-min.js
@@ -1,11 +1,19 @@
-module.exports = function (gulp, plugins, config, errorHandler) {
+module.exports = function(gulp, plugins, config) {
 	gulp.task('image-min', function() {
 		return gulp.src(config.paths.input.images)
-			.pipe(plugins.plumber(errorHandler))
 			.pipe(plugins.imagemin([
 				plugins.imageminJpegRecompress({
 					quality: 'low'
-				})
+				}),
+				plugins.imageminPngquant({
+                speed: 1,
+                quality: 75 //lossy settings
+            	}),
+				plugins.imageminZopfli({
+                	more: true
+            	}),
+				plugins.imagemin.svgo({plugins: [{removeViewBox: true}]}),
+				plugins.imagemin.gifsicle({interlaced: true})
 			]))
 			.pipe(gulp.dest(config.paths.output.images));
 	});
