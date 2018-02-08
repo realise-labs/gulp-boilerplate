e88f44aeee71d83987ea83b1b51b69d718fd40d3
David Storey <david.storey@realise.com>
2018-02-08 12:24:21 +0000

    Added an example of eslint-disable-next-line
    Useful for ignoring eslint issues that can't be resolved

--- src/scripts/module.js
+++ src/scripts/module.js
@@ -3,5 +3,8 @@
 'use strict';
 
 export default function hello(name) {
+	// eslint-disable-next-line
 	console.log('Hello ' + name);
+	// Normally this line ^^^ would get flagged by eslint
+	// But the comment line before tells eslint to ignore it
 }
