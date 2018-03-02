a06f418d15ad4a64b1afb64281a0f264f78b7315
David Storey <david.storey@realise.com>
2018-03-02 15:14:24 +0000

    Added caching for faster file watching
    Addresses https://github.com/realise-labs/gulp-boilerplate/issues/50

--- gulp-tasks/copy-dev.js
+++ gulp-tasks/copy-dev.js
@@ -2,6 +2,7 @@ module.exports = function (gulp, plugins, config, errorHandler) {
     gulp.task('copy-dev', function() {
         //copy static files
         gulp.src(config.paths.input.staticDev, { nodir: true })
+        .pipe(plugins.cached('copy-dev'))
         .pipe(plugins.plumber(errorHandler))
         .pipe(gulp.dest(config.paths.output.devRoot));
     });
