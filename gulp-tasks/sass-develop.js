35c0a683d3296be03e426d7f1a02d05e595329f1
David Storey <david.storey@realise.com>
2018-03-23 09:04:54 +0000

    Fixing SASS cache issue
    https://github.com/realise-labs/gulp-boilerplate/issues/55

--- gulp-tasks/sass-develop.js
+++ gulp-tasks/sass-develop.js
@@ -1,7 +1,6 @@
 module.exports = function (gulp, plugins, config, errorHandler) {
 	gulp.task('sass-develop', ['scss-lint'], function() {
 		return gulp.src(config.paths.input.styles)
-			.pipe(plugins.cached('sass-develop'))
 			.pipe(plugins.plumber(errorHandler))
 			.pipe(plugins.sass())
 			.pipe(gulp.dest(config.paths.output.styles));
