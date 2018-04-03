69df1bd25b898968469149ec2ac618a458abb585
Bee Flaherty <beeflaherty@gmail.com>
2018-04-03 14:25:04 +0100

    Issue Fix: Error when adding SVG during development #53

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -3,7 +3,6 @@ module.exports = function (gulp, plugins, config, errorHandler) {
 		gulp.watch(config.paths.input.staticDev,  ['copy-dev']);
 		gulp.watch(config.paths.input.styles, ['sass-develop']);
 		gulp.watch(config.paths.input.spriteSrc, ['sprite-create']);
-		gulp.watch(config.paths.input.images + '**/*.svg', ['svg2png']);
 		gulp.watch(config.paths.input.scripts + '**/*.js', function () {
 			plugins.runSequence(['es-lint', 'babelify-develop'], 'browser-reload');
 		});
