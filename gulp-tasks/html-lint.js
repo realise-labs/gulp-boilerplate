69c309c603b421c8de2a2a9108839cf574dbc902
David Storey <david.storey@realise.com>
2018-04-26 16:54:46 +0100

    Added flag for turning linting on/off

--- gulp-tasks/html-lint.js
+++ gulp-tasks/html-lint.js
@@ -1,34 +1,41 @@
 module.exports = function (gulp, plugins, config, errorHandler) {
 	var reload = plugins.browserSync.reload;
+
 	gulp.task('html-lint', function() {
-		return gulp.src(config.paths.output.html)
-			.pipe(plugins.cached('html-lint'))
-			.pipe(plugins.plumber(errorHandler))
-	        .pipe(plugins.htmllint({
-	        	'rules' : {
-					'attr-name-ignore-regex': ':',
-					'attr-name-style' : 'dash',
-					'attr-quote-style': 'double',
-					'attr-no-dup': true,
-					'class-no-dup': true,
-	        		'doctype-first' : true,
-	        		'doctype-html5' : true,
-					'head-req-title': true,
-	        		'html-req-lang' : true,
-	        		'id-class-style' : 'dash',
-					'id-no-dup': true,
-	        		'img-req-alt' : 'allownull',
-					'img-req-src': true,
-					'indent-style': 'tabs',
-					'input-req-label' : true,
-	        		'line-end-style' : false,
-					'tag-close': true,
-					'tag-name-lowercase': true,
-					'title-no-dup': true
-	        	},
-	        	'failOnError' : false
-			}, htmllintReporter))
-	        .pipe(reload({ stream: true}));
+		var files = gulp.src(config.paths.output.html)
+			.pipe(plugins.cached('html-lint'));
+
+		if (config.linting === true) {
+			return files
+				.pipe(plugins.plumber(errorHandler))
+				.pipe(plugins.htmllint({
+					'rules': {
+						'attr-name-ignore-regex': ':',
+						'attr-name-style': 'dash',
+						'attr-quote-style': 'double',
+						'attr-no-dup': true,
+						'class-no-dup': true,
+						'doctype-first': true,
+						'doctype-html5': true,
+						'head-req-title': true,
+						'html-req-lang': true,
+						'id-class-style': 'dash',
+						'id-no-dup': true,
+						'img-req-alt': 'allownull',
+						'img-req-src': true,
+						'indent-style': 'tabs',
+						'input-req-label': true,
+						'line-end-style': false,
+						'tag-close': true,
+						'tag-name-lowercase': true,
+						'title-no-dup': true
+					},
+					'failOnError': false
+				}, htmllintReporter))
+				.pipe(reload({ stream: true }));
+		} else {
+			return files;
+		}
 	});
 
 	function htmllintReporter(filepath, issues) {
