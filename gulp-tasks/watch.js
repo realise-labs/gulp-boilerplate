ff01780dea88f066d99529cb519d7154316e545c
David Storey <david.storey@realise.com>
2018-05-11 12:30:04 +0100

    Same change - missed it from Watch

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -1,7 +1,7 @@
 module.exports = function (gulp, plugins, config, errorHandler) {
 	gulp.task('watch', function() {
 		gulp.watch(config.paths.input.static,  ['copy-dev']);
-		gulp.watch(config.paths.input.styles, ['sass-develop']);
+		gulp.watch(config.paths.input.styles, ['scss-lint', 'sass-develop']);
 		gulp.watch(config.paths.input.spriteSrc, ['sprite-create']);
 		gulp.watch(config.paths.input.scripts + '**/*.js', function () {
 			plugins.runSequence(['es-lint', 'babelify-develop'], 'browser-reload');
